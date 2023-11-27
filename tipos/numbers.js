31a228fe633ce9d5bdd120a7dde43447bd5537ed
renier1989 <renier1989@gmail.com>
2023-11-27 12:52:00 -0500

    16. Numbers - Números

---
+++ tipos/numbers.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    let losBuenos = 10;
+    console.log({ losBuenos });
+    let losMalos = 40;
+    console.log({ losMalos });
+    if (losBuenos < losMalos) {
+        console.log('Perdimos la batalla');
+    }
+    else {
+        console.log('Ganamos la batalla');
+    }
+    losBuenos = Number('33dd');
+    console.log(losBuenos); // -> NaN esto es considerado como un numero
+})();
