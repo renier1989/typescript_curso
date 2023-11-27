9a5ebc430da26fbbcb9cb6c3cd5813e3b0e543e1
renier1989 <renier1989@gmail.com>
2023-11-27 15:29:23 -0500

    20. Tuples - Tuplas

---
+++ tipos/tuples.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    // con esto estoy dando la estructura que va a tener el arreglo.
+    // su primero valor debe ser un string y el segundo un numero
+    const superheroes = ["superman", 100];
+    //   modifico los valores en las posiciones 0 y 1
+    superheroes[0] = "batman";
+    superheroes[1] = 1;
+    //   superheroes[0] = 2;
+    //   superheroes[1] = 'algo';
+    superheroes.forEach((info) => console.log(info));
+})();
