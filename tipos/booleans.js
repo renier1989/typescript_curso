1eb97ca8f2f5403465d6cf3975f5269190415cfe
renier1989 <renier1989@gmail.com>
2023-11-27 12:39:42 -0500

    15. Booleans - Booleanos

---
+++ tipos/booleans.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    let isTimeToLunch = false;
+    let isTimeToGo;
+    isTimeToGo = true;
+    isTimeToLunch = (isTimeToGo) ? true : false;
+    console.log({ isTimeToLunch });
+})();
---
+++ tipos/booleans.ts
@@ -0,0 +1,10 @@
+(() => {
+  let isTimeToLunch: boolean = false;
+    let isTimeToGo:boolean;
+
+    isTimeToGo = true
+
+    isTimeToLunch = (isTimeToGo) ? true : false
+
+  console.log({isTimeToLunch});
+})();
