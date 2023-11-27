a34f226b8e92c704b6d9a6eef3c4dfd56ce32c4f
renier1989 <renier1989@gmail.com>
2023-11-27 12:28:26 -0500

    14. Inferir tipos y modo estricto

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 "use strict";
-const msg = "hola mundo";
-const hero = {
-    name: 'superman',
-    age: 34
-};
-hero.age = 50;
-console.log(hero.age + 1);
+// esto es una funcion auto invocada, para evitar choque con las definiciones de variables globales de otros archivos
+(() => {
+    const nombre = "Renier Vargas";
+    console.log(nombre);
+    const a = "hola mundo";
+    let b = 4;
+    function saludar(msg) {
+        console.log(msg.toUpperCase());
+    }
+    saludar('Venezuela');
+})();
+// const  msg = "hola mundo";
+// const hero = {
+//     name : 'superman',
+//     age: 34
+// }
+// hero.age = 50
+// console.log(hero.age + 1 );
--- app.ts
+++ app.ts
@@ -1,8 +1,21 @@
-const  msg = "hola mundo";
-const hero = {
-    name : 'superman',
-    age: 34
-}
-hero.age = 50
-console.log(hero.age + 1 );
+// esto es una funcion auto invocada, para evitar choque con las definiciones de variables globales de otros archivos
+(() => {
+  const nombre: string = "Renier Vargas";
+  console.log(nombre);
+  const a: string = "hola mundo";
+  let b = 4;
+  
+  function saludar(msg: string) {
+    console.log(msg.toUpperCase());
+  }
+  saludar('Venezuela')
+})();
 
+
+// const  msg = "hola mundo";
+// const hero = {
+//     name : 'superman',
+//     age: 34
+// }
+// hero.age = 50
+// console.log(hero.age + 1 );
