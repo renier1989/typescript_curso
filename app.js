58478ba0c893e3e4350ccd7ebdb06a7daa321443
renier1989 <renier1989@gmail.com>
2023-11-27 12:07:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = "hola mundo";
+const hero = {
+    name: 'superman',
+    age: 34
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ app.ts
@@ -0,0 +1,8 @@
+const  msg = "hola mundo";
+const hero = {
+    name : 'superman',
+    age: 34
+}
+hero.age = 50
+console.log(hero.age + 1 );
+
