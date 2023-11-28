02f8398e9aabbdc29221042bf7fb34fc80feed41
Renier Vargas <renier1989@gmail.com>
2023-11-27 20:47:58 -0500

    36. Objetos básicos

---
+++ objetos/objects.ts
@@ -0,0 +1,20 @@
+(()=>{
+
+    // la primera declaracion es la que se hace la deficion de los tipos internos del objeto
+    let superHeroe = {
+        nombre : 'Bruce Wayne',
+        alias : 'Batman',
+        edad : 30,
+        poderes : ['Inteligencia','Millonario']
+    }
+
+    // esta reasignacion de valores debe seguir la primera definicion de tipos que se hizo en primer lugar
+    superHeroe = {
+        nombre: 'Clark Kent',
+        alias : 'Superman',
+        edad: 33,
+        poderes: ['Vision RayosX', 'Super Fuerza']
+    }
+
+
+})()
