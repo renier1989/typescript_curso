2c9b2b6e157d1cbcb327472e9340037d9f9c25a9
Renier Vargas <renier1989@gmail.com>
2023-11-29 19:23:51 -0500

    61. Métodos públicos y privados

--- classes/basica.ts
+++ classes/basica.ts
@@ -10,17 +10,32 @@
     //     this.team = team;
     //     this.realName = realName;
     // }
-    
+
     static age: number = 33;
+    static getAvgAge(){
+
+        return `${this.name} - ${this.age}`; // esto accede al nombre pero de la clase o lo que esta definido como static, no a las propiedades del constructor.
+
+    }
+
     constructor(
       private name: string,
       private team: string = "texto opcional",
       public realName?: string
     ) {}
+
+    // metodo publico
+    bio(){
+        return `${this.name} (${this.team})`;
+    }
   }
 
   const antman: Personaje = new Personaje("AntMan", "Vengadores");
   console.log(antman);
   // las propiedades static se acceden haciendo la referencia a la clase directamente
-  // console.log(Personaje.age);
+  console.log(Personaje.age);
+
+  console.log(antman.bio());
+  console.log(Personaje.getAvgAge());
+  
 })();
--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 "use strict";
 (() => {
     class Personaje {
+        static getAvgAge() {
+            return `${this.name} - ${this.age}`;
+        }
         constructor(name, team = "texto opcional", realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Personaje.age = 33;
     const antman = new Personaje("AntMan", "Vengadores");
     console.log(antman);
+    console.log(Personaje.age);
+    console.log(antman.bio());
+    console.log(Personaje.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
