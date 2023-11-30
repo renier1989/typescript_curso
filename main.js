540fd43488478ba8832cf90465b6fe469ecde0e7
Renier Vargas <renier1989@gmail.com>
2023-11-29 19:13:18 -0500

    60. Forma corta de asignar propiedades

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 "use strict";
 (() => {
     class Personaje {
-        constructor(name, team, realName) {
+        constructor(name, team = "texto opcional", realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
     }
     Personaje.age = 33;
-    const antman = new Personaje('AntMan', 'Vengadores');
+    const antman = new Personaje("AntMan", "Vengadores");
     console.log(antman);
 })();
 //# sourceMappingURL=main.js.map
