f5091793da88194f7079ce604f6fe8fe5b599f10
Renier Vargas <renier1989@gmail.com>
2023-11-30 21:36:45 -0500

    77. Creando un Namespace

--- main.js
+++ main.js
@@ -1,94 +1,12 @@
 "use strict";
-(() => {
-    class Mutante {
-        constructor(nombre, rol) {
-            this.nombre = nombre;
-            this.rol = rol;
-        }
-    }
-    class Bueno extends Mutante {
-        salvar() {
-            return `${this.nombre} tiene que salvar personas`;
-        }
-    }
-    class Malo extends Mutante {
-        destruir() {
-            return `${this.nombre} tiene que destruir cosas`;
-        }
-    }
-    const ciclope = new Bueno('Ciclope', 'Bueno');
-    const magneto = new Malo('Magneto', 'Malo');
-})();
-(() => {
-    class Personaje {
-        static getAvgAge() {
-            return `${this.name} - ${this.age}`;
-        }
-        constructor(name, team = "texto opcional", realName) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
-        bio() {
-            return `${this.name} (${this.team})`;
-        }
-    }
-    Personaje.age = 33;
-    const antman = new Personaje("AntMan", "Vengadores");
-})();
-(() => {
-    class Hero {
-        constructor(name, realName) {
-            this.name = name;
-            this.realName = realName;
-            console.log("esto es el contructor Hero");
-        }
-        getFullName() {
-            return `${this.name} ${this.realName}`;
-        }
-        get fullName() {
-            return `algo desde el get : ${this.name} ${this.realName}`;
-        }
-        set fullName(name) {
-            if (name.length < 3) {
-                throw new Error("El nombre es muy corto!!!");
-            }
-            this.name = name;
-        }
-    }
-    class Xmen extends Hero {
-        constructor(name, realName, isMutant) {
-            super(name, realName);
-            this.isMutant = isMutant;
-            console.log("esto es el contructor Xmen");
-        }
-        getFullNameFromXmen() {
-            console.log(super.getFullName());
-        }
-    }
-})();
-(() => {
-    class Starwars {
-        constructor(pelicula, nombre) {
-            this.pelicula = pelicula;
-            this.nombre = nombre;
-        }
-        static obtenerPelicula() {
-            if (!Starwars.instance) {
-                Starwars.instance = new Starwars('Episodio 3', 'La venganza de los sith');
-            }
-            return Starwars.instance;
-        }
-        cambiarPelicula(otraPelicula) {
-            this.pelicula = otraPelicula;
-        }
-    }
-    const pelicula = Starwars.obtenerPelicula();
-    const pelicula2 = Starwars.obtenerPelicula();
-    console.log(pelicula, pelicula2);
-    pelicula.cambiarPelicula('Harry Potter');
-    pelicula2.cambiarPelicula('Interstellar');
-    console.log(pelicula);
-    console.log(pelicula2);
-})();
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length < 3) ? false : true;
+    };
+    const validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
+    };
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Re'));
 //# sourceMappingURL=main.js.map
