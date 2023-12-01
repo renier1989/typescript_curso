4f9b79359c5a9b62666ef5449b5b0faaabe7725c
Renier Vargas <renier1989@gmail.com>
2023-11-30 19:58:52 -0500

    69. Estructuras complejas

---
+++ interfaces/complejas.ts
@@ -0,0 +1,38 @@
+(() => {
+  type IdUuid = `${string}-${string}-${string}-${string}-${string}`;
+  interface Client {
+    id: IdUuid;
+    name: string;
+    age: number;
+    address?: Address;
+    // address?: {
+    //   id: number;
+    //   zip: string;
+    //   city: string;
+    // };
+  }
+
+  // cuando hay mas propiedades anidadas se recomienda crear una nueva interface
+  interface Address {
+    id: number;
+    zip?: string;
+    city: string;
+  }
+
+  const client: Client = {
+    id: crypto.randomUUID(),
+    name: "Renier",
+    age: 36,
+    address: {
+        id: 123,
+        zip: '123-abc',
+        city: 'Venezuela'
+    }
+  };
+
+  const client2:Client = {
+    id: crypto.randomUUID(),
+    name: 'Nancy',
+    age: 36,
+  }
+})();
