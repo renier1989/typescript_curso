115776764d2f926a7a88ac6717ee2f5043230f2b
Renier Vargas <renier1989@gmail.com>
2023-12-02 08:16:13 -0500

    85 - Funciones Genericas

--- ts-node/src/generics/generics.ts
+++ ts-node/src/generics/generics.ts
@@ -2,6 +2,12 @@ export const printAlgo = (argumento:any):void =>{
     console.log(argumento);
 }
 
-export function genericFunciton(argument:any){
+// esto es una funcion normal sin tipado generico
+export function genericFuncitonNormal(argument:any){
 return argument;
+}
+
+// esto es una funcion generica que retorna el mismo tipo de lo que recibe la funcion
+export function genericFunction<T>(argument:T){
+    return argument;
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,4 +1,4 @@
-import { genericFunciton, printAlgo } from "./generics/generics";
+import { genericFuncitonNormal, genericFunction, printAlgo } from "./generics/generics";
 
 
 // // funcion generica basica porque recibe cualquier tipo de valor any
@@ -10,7 +10,17 @@ import { genericFunciton, printAlgo } from "./generics/generics";
 // printAlgo(crypto.randomUUID());
 // printAlgo([1,2,3,4,5,6,7,8,9,10,11]);
 
-console.log( genericFunciton(3.1434234234).toFixed(2) );
-console.log( genericFunciton('Renier Vargas').toFixed(2) );
-console.log( genericFunciton(new Date()).toFixed(2) );
+// console.log( genericFuncitonNormal(3.1434234234).toFixed(2) );
+// console.log( genericFuncitonNormal('Renier Vargas').toFixed(2) ); // Error
+// console.log( genericFuncitonNormal(new Date()).toFixed(2) ); // Error
+// console.log( genericFuncitonNormal(3.1434234234).toFixed(2) );
+
+
+// ejemplo de funcion genericas
+const name:string = 'Renier Vargas'; 
+const someNumber:number = 234.2343424;
+
+console.log( genericFunction(name).toUpperCase() ); 
+console.log( genericFunction(new Date()).getFullYear() ); 
+console.log( genericFunction(someNumber).toFixed(2) ); 
 
