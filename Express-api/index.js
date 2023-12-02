35271ddbf02ac0cb099cdb27f4b9dda6a127df48
Renier Vargas <renier1989@gmail.com>
2023-12-02 12:59:17 -0500

    102. Creando un Rest API con Express updated

--- Express-api/index.js
+++ Express-api/index.js
@@ -1,1 +1,14 @@
-console.log('hola como estas ?');
+"use strict";
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.json({
+        id: 1,
+        status: 200,
+        nombre: 'Renier Vargas',
+    });
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ Express-api/index.ts
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    id: 1,
+    status : 200,
+    nombre: 'Renier Vargas',
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
