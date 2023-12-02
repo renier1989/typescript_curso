7fa9f591e463e009962cb4a1de942d8c31bdcd9e
Renier Vargas <renier1989@gmail.com>
2023-12-02 13:14:33 -0500

    103. Trabajar con TypeScript en lugar de JavaScript

---
+++ Express-api/dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// const express = require('express')
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.status(401).json({
+        error: 'algo',
+        msg: 'no tengo un respuesta valida'
+    });
+    // res.json({
+    //   id: 1,
+    //   status : 200,
+    //   nombre: 'Renier Vargas',
+    // })
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- Express-api/index.ts
+++ Express-api/index.ts
@@ -1,13 +1,21 @@
-const express = require('express')
+import express from 'express'
+// const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.json({
-    id: 1,
-    status : 200,
-    nombre: 'Renier Vargas',
+
+  res.status(401).json({
+    error:'algo',
+    msg: 'no tengo un respuesta valida'
   })
+
+  // res.json({
+  //   id: 1,
+  //   status : 200,
+  //   nombre: 'Renier Vargas',
+  // })
+
 })
 
 app.listen(port, () => {
