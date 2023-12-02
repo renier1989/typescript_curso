492528588c63f3360a569f83c76f4fce4547b0fa
Renier Vargas <renier1989@gmail.com>
2023-12-02 12:07:00 -0500

    97. Decoradores de métodos

--- ts-node/src/decorators/starwars.ts
+++ ts-node/src/decorators/starwars.ts
@@ -15,12 +15,40 @@ const bloquearPrototipo = function(constructor: Function){
     Object.seal(constructor.prototype)
 }
 
-// los decoradores se van ejecutando en order de decalracion
 
+// esto es un factory decorator que siempre retorna otra funcion
+function CheckValidDataId(){
+    // target apunta a constructor de la clase
+    // key es el nombre a lo que esta decorando
+    // descriptor es la parte de los argumentos que se le pasan al metodo
+    return function(target: any, key: String, descriptor: PropertyDescriptor){
+        // console.log('aqui');
+        // console.log(target, key, descriptor);
+        // console.log(descriptor.value);
+        const self = descriptor.value;
+        descriptor.value = (id:number)=>{
+            if(id < 1 || id > 100){
+                return console.error('El Id es invalidad, debe estar entre 1 y 100');
+            }else{
+                return self(id)
+            }
+        }
+        
+    }
+}
+
+
+
+// los decoradores se van ejecutando en order de decalracion
 // @printLogScreen
-@bloquearPrototipo
-@printContructorInScreen()
+// @bloquearPrototipo
+// @printContructorInScreen()
 export class PersonajeStarwars {
   public autor: string = "George Lucas";
   constructor(public nombre: string) {}
+
+  @CheckValidDataId()
+  saveDataInDB(dataId:number){
+    console.log(`los datos fueron registrados con exito : ${dataId}`);
+  }
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -3,10 +3,10 @@ import { PersonajeStarwars } from "./decorators/starwars";
 
 const personaje = new PersonajeStarwars('Padme Amidala');
 // console.log(personaje);
-
-
 // personaje.nombre = 'Maul'
-PersonajeStarwars.prototype.nombre = 'Maul'
+// PersonajeStarwars.prototype.nombre = 'Maul'
 // (PersonajeStarwars.prototype as any).customName =  'algo mas';
+// console.log(personaje);
 
-console.log(personaje);
+personaje.saveDataInDB(3);
+personaje.saveDataInDB(1000);
