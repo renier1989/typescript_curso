359c3e8386fe3ac98eecfafd9984dac4d9ec01f6
Renier Vargas <renier1989@gmail.com>
2023-12-02 12:33:30 -0500

    98. Decoradores de propiedades

--- ts-node/src/decorators/starwars.ts
+++ ts-node/src/decorators/starwars.ts
@@ -10,45 +10,64 @@ const printContructorInScreen = (print: boolean = false): Function => {
   return () => console.log("hola desde un decorador arrow function");
 };
 
-const bloquearPrototipo = function(constructor: Function){
-    Object.seal(constructor)
-    Object.seal(constructor.prototype)
-}
-
+const bloquearPrototipo = function (constructor: Function) {
+  Object.seal(constructor);
+  Object.seal(constructor.prototype);
+};
 
 // esto es un factory decorator que siempre retorna otra funcion
-function CheckValidDataId(){
-    // target apunta a constructor de la clase
-    // key es el nombre a lo que esta decorando
-    // descriptor es la parte de los argumentos que se le pasan al metodo
-    return function(target: any, key: String, descriptor: PropertyDescriptor){
-        // console.log('aqui');
-        // console.log(target, key, descriptor);
-        // console.log(descriptor.value);
-        const self = descriptor.value;
-        descriptor.value = (id:number)=>{
-            if(id < 1 || id > 100){
-                return console.error('El Id es invalidad, debe estar entre 1 y 100');
-            }else{
-                return self(id)
-            }
-        }
-        
-    }
+function CheckValidDataId(): Function {
+  // target apunta a lo que se esta decorando
+  // key es el nombre a lo que esta decorando
+  // descriptor es la parte de los argumentos que se le pasan al metodo
+  return function (target: any, key: String, descriptor: PropertyDescriptor) {
+    // console.log('aqui');
+    // console.log(target, key, descriptor);
+    // console.log(descriptor.value);
+    const self = descriptor.value;
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 100) {
+        return console.error("El Id es invalidad, debe estar entre 1 y 100");
+      } else {
+        return self(id);
+      }
+    };
+  };
 }
 
+function readOnly(isWriteAble: boolean = true): Function {
+  return (targe: any, key: string) => {
+    const  descriptor: PropertyDescriptor = {
+        get(){
+            console.log(this, 'getter');
+            return 'Renier';
+        },
+        set(this , val){
+            // console.log(this,val,key)
+            Object.defineProperty(this,key,{
+                value:val,
+                writable: !isWriteAble,
+                enumerable:false
+            })
+        }
+    }
 
+    return descriptor
+
+  };
+}
 
 // los decoradores se van ejecutando en order de decalracion
 // @printLogScreen
 // @bloquearPrototipo
 // @printContructorInScreen()
 export class PersonajeStarwars {
+    @readOnly(true)
   public autor: string = "George Lucas";
   constructor(public nombre: string) {}
 
   @CheckValidDataId()
-  saveDataInDB(dataId:number){
+  saveDataInDB(dataId: number) {
     console.log(`los datos fueron registrados con exito : ${dataId}`);
   }
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -2,11 +2,15 @@ import { PersonajeStarwars } from "./decorators/starwars";
 
 
 const personaje = new PersonajeStarwars('Padme Amidala');
-// console.log(personaje);
+// const au = personaje.autor = 'hola' // aunque TS se queda y nos advierte que es una propiera privada, en JS si se puede modificar
+
+personaje.autor = 'Renier Vargas'
+console.log(personaje);
+
 // personaje.nombre = 'Maul'
 // PersonajeStarwars.prototype.nombre = 'Maul'
 // (PersonajeStarwars.prototype as any).customName =  'algo mas';
 // console.log(personaje);
 
-personaje.saveDataInDB(3);
-personaje.saveDataInDB(1000);
+// personaje.saveDataInDB(3);
+// personaje.saveDataInDB(1000);
